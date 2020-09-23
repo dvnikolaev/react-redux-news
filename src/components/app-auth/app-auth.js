caae1d7c3e1863578d5c320191a72a7668d68557
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-23 16:55:55 +0400

    add auth modal

--- src/components/app-auth/app-auth.js
+++ src/components/app-auth/app-auth.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
 import "./app-auth.css";
+import AuthModal from "./auth-modal/auth-modal";
 
 const Auth = ({ isAuth }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const renderAuthModal = () => {
+    if (!isOpen) {
+      return null;
+    }
+    return <AuthModal setIsOpen={setIsOpen} />;
+  };
+
   const renderButtonAuth = () => {
     if (isAuth) {
       return <button className="button button--error">Выйти</button>;
-    } else {
-      return <button className="button button--success">Войти</button>
     }
+    return (
+      <button
+        className="button button--success"
+        onClick={() => setIsOpen(true)}
+      >
+        Войти
+      </button>
+    );
   };
 
-  return <div className="auth">{renderButtonAuth()}</div>;
+  return (
+    <div className="auth">
+      {renderButtonAuth()}
+      {`${isOpen}`}
+      {renderAuthModal()}
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
---
+++ src/components/app-auth/auth-modal/auth-modal.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Modal from "../../Modal/Modal";
+
+const AuthModal = ({ setIsOpen }) => {
+  return <Modal setIsOpen={setIsOpen}>aasd</Modal>;
+};
+
+export default AuthModal;
