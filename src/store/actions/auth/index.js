6a6dd88380be2cd6c679af1fd72c4e9b93403cc7
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-23 21:00:22 +0400

    feat: add onLogin, onLogout, setErrorAuth

---
+++ src/store/actions/auth/index.js
@@ -0,0 +1,42 @@
+import { store } from "../../index";
+
+import { LOGIN, LOGOUT, SET_ERROR_AUTH } from "./types";
+
+export const onLogin = (username, password) => {
+  const foundedUser = store.getState().auth.users.find((user) => {
+    return (
+      username.toLowerCase() === user.login.toLowerCase() &&
+      password === user.password
+    );
+  });
+
+  if (foundedUser) {
+    return {
+      type: LOGIN,
+      isAuth: true,
+      username
+    };
+  }
+  
+  store.dispatch(setErrorAuth('Неправильный логин или пароль'));
+
+  return {
+    type: LOGIN,
+    isAuth: false,
+    username: ''
+  }
+
+};
+
+export const onLogout = () => {
+  return {
+    type: LOGOUT
+  }
+}
+
+export const setErrorAuth = text => {
+  return {
+    type: SET_ERROR_AUTH,
+    text
+  }
+}
