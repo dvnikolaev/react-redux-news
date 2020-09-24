8ec6f6803a81c10e93699e1a8fd7b2dbb766232a
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-24 23:16:39 +0400

    add login info

--- src/pages/Main/page-main.js
+++ src/pages/Main/page-main.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import './page-main.css';
+import "./page-main.css";
 
 const Main = ({ username }) => {
-  let name = username ? username : "гость";
+  const name = username ? username : "гость";
 
-  return <h3>Привет, <span className="name">{name}!</span></h3>;
+  const renderInfo = () => {
+    if (username) {
+      return null;
+    }
+
+    return (
+      <>
+        <p>Для входа в систему используйте пары логин - пароль:</p>
+        <p>user - user</p>
+        <p>admin - admin</p>
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <h3>
+        Привет, <span className="name">{name}!</span>
+      </h3>
+      {renderInfo()}
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
