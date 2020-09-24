6fc1dbe239e6e86192936f9d2bf5befac2919a73
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-24 19:12:23 +0400

    add formInput

--- src/components/app-auth/auth-modal/auth-modal.js
+++ src/components/app-auth/auth-modal/auth-modal.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 
-import "./auth-modal.css";
+import "../../form-parts/form-parts.css";
 import Modal from "../../Modal/Modal";
+import FormInput from "../../form-parts/form-input";
 import { onLogin } from "../../../store/actions/auth";
 
 const AuthModal = ({ setIsOpen, onLogin, errorAuth }) => {
@@ -21,33 +22,27 @@ const AuthModal = ({ setIsOpen, onLogin, errorAuth }) => {
       return null;
     }
 
-    return <span className="auth__error">{errorAuth}</span>;
+    return <span className="text__error">{errorAuth}</span>;
   };
 
   return (
     <Modal setIsOpen={setIsOpen}>
       <h2 className="form__header">Вход в систему</h2>
       <form className="form" onSubmit={onSubmitForm}>
-        <label className="form__label">
-          <span className="form__text">Имя пользователя:</span>
-          <input
-            name="username"
-            type="text"
-            className="form__input"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label className="form__label">
-          <span className="form__text">Пароль:</span>
-          <input
-            className="form__input"
-            name="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
+        <FormInput
+          label="имя пользователя"
+          value={username}
+          setValue={setUsername}
+          type="text"
+          name="username"
+        />
+        <FormInput 
+          label="пароль"
+          value={password}
+          setValue={setPassword}
+          type="password"
+          name="password"
+        />
         {renderError()}
         <button
           className="button form__button form__button-enter button--success"
