cc6abdd925722e41a16c93cf38c45d289564308c
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-24 23:54:36 +0400

    Updates

---
+++ precache-manifest.5bdbfb0436739879e99d57841c597052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff638f12dc697ee00c8550c2fa6d5fa",
+    "url": "/react-redux-news/index.html"
+  },
+  {
+    "revision": "f7100232892774b77bee",
+    "url": "/react-redux-news/static/css/main.c8013a1b.chunk.css"
+  },
+  {
+    "revision": "41d27eff8931b1126d23",
+    "url": "/react-redux-news/static/js/2.4ad207a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-news/static/js/2.4ad207a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7100232892774b77bee",
+    "url": "/react-redux-news/static/js/main.3259750a.chunk.js"
+  },
+  {
+    "revision": "cf7ab6c10083d96dec91",
+    "url": "/react-redux-news/static/js/runtime-main.673293bf.js"
+  }
+]);
