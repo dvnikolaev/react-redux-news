74bdaf60477de7657d4fdffa9ab82e340be6c552
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-24 12:34:17 +0400

    add isNotAccept news'

--- src/store/reducers/news.js
+++ src/store/reducers/news.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
   news: [
     {
       id: 1,
-      author_id: "1",
+      author_id: 1,
       title: "Lorem, ipsum.",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, aliquid.",
@@ -12,14 +12,23 @@ const INITIAL_STATE = {
       isAccept: true,
     },
     {
-      id: 1,
-      author_id: "1",
+      id: 2,
+      author_id: 1,
       title: "Expedita provident eligendi.",
       description:
         "Expedita provident eligendi repellendus corrupti culpa soluta laboriosam sint?",
       date: "2020-09-23",
       isAccept: true,
     },
+    {
+      id: 3,
+      author_id: 1,
+      title: "de Finibus Bonorum et Malorum.",
+      description:
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis",
+      date: "2020-09-23",
+      isAccept: false,
+    }
   ],
 };
 
